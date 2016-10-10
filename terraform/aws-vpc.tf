be1e3e6525eaa5a437a63f0289f5736478db9fba
Andrew Langhorn <andrew.langhorn@thoughtworks.com>
2016-10-10 16:26:58 +0100

    Fix typo: s/zons/zones

--- terraform/aws-vpc.tf
+++ terraform/aws-vpc.tf
@@ -9,11 +9,11 @@ resource "aws_vpc" "dotcom" {
 resource "aws_subnet" "dotcom-private-a" {
   vpc_id = "${aws_vpc.dotcom.id}"
   cidr_block = "10.0.1.0/24"
-  availability_zone = "${data.aws_availability_zons.available.names[0]}"
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
 }
 
 resource "aws_subnet" "dotcom-private-b" {
   vpc_id = "${aws_vpc.dotcom.id}"
   cidr_block = "10.0.2.0/24"
-  availability_zone = "${data.aws_availability_zons.available.names[1]}"
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
 }
