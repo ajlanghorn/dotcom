ae03fbd13b634cc145b3b8e11024b0d080fddcf2
Andrew Langhorn <andrew.langhorn@thoughtworks.com>
2016-10-10 15:25:23 +0100

    Add VPC, subnets and AZ data sources

---
+++ terraform/aws-vpc.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "dotcom" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = "true"
+  tags {
+    Name = "dotcom"
+  }
+}
+
+resource "aws_subnet" "dotcom-private-a" {
+  vpc_id = "${aws_vpc.dotcom.id}"
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "${data.aws_availability_zons.available.names[0]}"
+}
+
+resource "aws_subnet" "dotcom-private-b" {
+  vpc_id = "${aws_vpc.dotcom.id}"
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "${data.aws_availability_zons.available.names[1]}"
+}
